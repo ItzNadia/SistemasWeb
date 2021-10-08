5febbc1dff1d585e2d47b252eb2c98c55c9f72cc
Nadia Bravo <nait0197@live.com.mx>
2021-10-08 01:20:30 -0500

    Practicas

---
+++ Practicas/formulario_2.js
@@ -0,0 +1,29 @@
+function boton4() {
+    window.alert('enviar4');
+}
+
+var btnEnviar1 = document.getElementById('btnEnviar1');
+btnEnviar1.style.color = "yellow";
+// se agrega un listener (escucha) para cachar el evento click en el botón
+btnEnviar1.addEventListener('click', function () {
+    window.alert('enviar1');
+    cambiarColor("red");
+    cambiarEstado(true);
+});
+
+var emailHelp = document.getElementById('emailHelp');
+function cambiarColor(color) {
+    emailHelp.style.color = color;
+}
+
+function cambiarEstado(estado) {
+    var exampleCheck1 = document.getElementById("exampleCheck1").checked = estado;
+}
+
+var btnEnviar5 = document.getElementById('btnEnviar5');
+btnEnviar5.addEventListener('click', callback);
+
+function callback() {
+    cambiarColor("green");
+    cambiarEstado(false);
+}
